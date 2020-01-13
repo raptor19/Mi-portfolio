c5d7270be6de1eddf1c4088b2fdad944d6c6e13d
Cristian <cristiannicolas0987@hotmail.com>
2020-01-13 00:52:00 -0300

    Mejoras en el Dom y ngx-Translate instalada
    Mejore la parte responsive de la app y corregi algunos errores gramaticales,tambien comenze la implementacion de cambio de idioma de la app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolioCristian';
+  langs: string[] = [];
+  
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+    this.translate.addLangs(['es', 'en']);
+    this.langs = this.translate.getLangs();
+  }
+
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
